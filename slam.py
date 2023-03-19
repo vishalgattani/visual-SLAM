e92bc643b8f41de7942b9f4aefb04a20fa6700e7
Vishal Gattani <vishalgattani09@gmail.com>
2023-03-18 23:24:01 -0400

    features detection extraction and matching using opencv

--- slam.py
+++ slam.py
@@ -16,12 +16,32 @@ H = 1080//2
 orb = cv2.ORB_create()
 
 
+class FeatureExtractor(object):
+    def __init__(self) -> None:
+        self.orb = cv2.ORB_create(100)
+        self.bf = cv2.BFMatcher()
+        self.last = None
 
+    def extract(self,img):
+        # detection
+        features = cv2.goodFeaturesToTrack(np.mean(img,axis=2).astype(np.uint8),3000,qualityLevel=0.01,minDistance=3)
+        # extraction
+        kps = [cv2.KeyPoint(x=f[0][0],y=f[0][1],size=20) for f in features]
+        kps,des = self.orb.compute(img,kps)
+        # matching
+        matches = None
+        if self.last is not None:
+            matches = self.bf.match(des,self.last['des'])
+
+        self.last = {"kps":kps,"des":des}
+        return kps,des,matches
+
+fe = FeatureExtractor()
 
 def process_frame(img):
     img = cv2.resize(img,(W,H))
     # find the keypoints and descriptors with ORB
-    kp, des = orb.detectAndCompute(img,None)
+    kp, des, matches = fe.extract(img)
     for p in kp:
         u,v = map(lambda u:int(round(u)),p.pt)
         cv2.circle(img,(u,v),color=(0,255,0),radius=3)
