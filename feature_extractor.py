e543d8084d9952b8717731aa1dac2be551954f69
Vishal Gattani <vishalgattani09@gmail.com>
2023-03-19 12:18:55 -0400

    normalize coords

--- feature_extractor.py
+++ feature_extractor.py
@@ -5,13 +5,26 @@ from skimage.feature import match_descriptors, ORB, plot_matches
 from skimage.measure import ransac
 from skimage.transform import FundamentalMatrixTransform
 
+def add_ones(x):
+   return  np.concatenate([x,np.ones((x.shape[0],1))],axis=1)
+
 class FeatureExtractor(object):
-    def __init__(self) -> None:
+    def __init__(self,K) -> None:
         self.orb = cv2.ORB_create(100)
         self.bf = cv2.BFMatcher(cv2.NORM_HAMMING)
         self.last = None
+        self.K = K
+        self.Kinv = np.linalg.inv(self.K)
+
+    def normalize(self,pts):
+        return np.dot(self.Kinv,add_ones(pts).T).T[:,0:2]
+
+    def denormalize(self,pt):
+        ret = np.dot(self.K,np.array([pt[0],pt[1],1.0]))
+        return int(round(ret[0])),int(round(ret[1]))
 
     def extract(self,img):
+
         # detection
         features = cv2.goodFeaturesToTrack(np.mean(img,axis=2).astype(np.uint8),3000,qualityLevel=0.01,minDistance=3)
         # extraction
@@ -27,15 +40,23 @@ class FeatureExtractor(object):
                     kp2 = self.last["kps"][m.trainIdx].pt
                     ret.append((kp1,kp2))
 
-        ret = np.array(ret)
+
 
         #filter
         # how ponts correspond to each other is governed by fundamental matrix
         # Estimate the epipolar geometry between the left and right image.
         if len(ret)>0:
+            ret = np.array(ret)
+
+            # normalize coordinates
+            ret[:,0,:] = self.normalize(ret[:,0,:])
+            ret[:,1,:] = self.normalize(ret[:,1,:])
+
             model, inliers = ransac((ret[:,0],ret[:,1]),
                             FundamentalMatrixTransform, min_samples=8,
                             residual_threshold=1, max_trials=1000)
             ret = ret[inliers]
+            fm = model.params
+            s,v,d = np.linalg.svd(fm)
         self.last = {"kps":kps,"des":des}
         return ret
--- slam.py
+++ slam.py
@@ -15,17 +15,22 @@ from feature_extractor import FeatureExtractor
 W = 1920//2
 H = 1080//2
 
+F = 1
+K = np.array([[F,0,W//2],[0,F,H//2],[0,0,1]])
 
-fe = FeatureExtractor()
+
+fe = FeatureExtractor(K)
+
 
 def process_frame(img):
     img = cv2.resize(img,(W,H))
     # find the keypoints and descriptors with ORB
     matches = fe.extract(img)
     print(len(matches),"matches")
+
     for p1,p2 in matches:
-        u1,v1 = map(lambda u:int(round(u)),p1)
-        u2,v2 = map(lambda u:int(round(u)),p2)
+        u1,v1 = fe.denormalize(p1)
+        u2,v2 = fe.denormalize(p2)
         cv2.circle(img,(u1,v1),color=(0,255,0),radius=3)
         cv2.line(img,(u1,v1),(u2,v2),color=(255,0,0))
     cv2.imshow("visual-SLAM",img)
